b74b9de591a28ddc34f1c63b2813e11e0ea5c7fd
rimaakter1 <rimac201262@gmail.com>
2025-02-20 15:07:08 +0600

    I do the basic setup first

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1>HELLLLLLLLLLLOOOOO</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/routes/router.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from 'react-router-dom'
+import MainLayout from '../layouts/MainLayout'
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+import ErrorPage from '../pages/ErrorPage'
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: '/',
+                element: <Home />,
+            },
+        
+            {
+                path: 'login',
+                element: <Login></Login>
+            },
+            {
+                path: 'register',
+                element: <Register></Register>
+            },
+        ],
+    },
+   
+
+
+])
